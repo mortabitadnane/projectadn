79b32f3d58e2b5d0f1e04ac0e0c886a11834f104
mortabitadnane <adnanemortabit16@gmail.com>
2023-08-11 11:32:57 +0100

    modifier

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,11 +1,11 @@
-<footer class="bg-gray-800 py-20 mt-20">
+<footer class="bg-gray-600 py-20 mt-20">
     <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-white">
                 Pages
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-gray-300">
                 <li class="pb-1">
                     <a href="/">
                         Home
@@ -30,11 +30,11 @@
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-white">
                 Find Us
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-gray-300">
                 <li class="pb-1">
                     <a href="/">
                         What we do
@@ -59,11 +59,11 @@
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-white">
                 Latest posts
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-gray-300">
                 <li class="pb-1">
                     <a href="/">
                         Why we love tech
